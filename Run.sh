92d7f28fcb6e89cc5a1adc5d4dcf26c4074c9a2c
Ellpeck <me@ellpeck.de>
2021-04-22 20:01:01 +0200

    fixed linux run script

--- Run.sh
+++ Run.sh
@@ -4,8 +4,8 @@ cd "$(dirname "$0")"
 # build the mod
 dotnet build
 # copy the mod to the mods folder
-cp ./bin/Debug/net5.0/* "$LOCALAPPDATA/Tiny Life/Mods" -r
+cp ./bin/Debug/net5.0/* "$HOME/.local/share/Tiny Life/Mods" -r
 # run the game
-dir=$(<"$LOCALAPPDATA/Tiny Life/GameDir")
+dir=$(<"$HOME/.local/share/Tiny Life/GameDir")
 cd $dir
-"./Tiny Life.exe"
+"./Tiny Life"
