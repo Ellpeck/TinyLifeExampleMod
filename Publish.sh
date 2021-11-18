82167f9e380dce2066c817537ae134c259d56030
Ellpeck <me@ellpeck.de>
2021-11-18 10:26:47 +0100

    bleh zip command

--- Publish.sh
+++ Publish.sh
@@ -7,5 +7,5 @@ dotnet build
 cd ./bin/Debug/net5.0
 file=$(find *.dll -printf %f)
 dest=${file::-4}.zip
-zip -r ../$dest * --exclude ref --exclude *.pdb --exclude *.deps.json
+zip -FSr ../$dest * --exclude 'ref/*' --exclude '*.pdb' --exclude '*.deps.json'
 echo Published to bin/Debug/$dest
